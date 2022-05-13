bce77b2007062b3c3179e4cb6140fbf7074ece75
mariogsanz <mariogsanz@hotmail.com>
2022-05-13 09:13:52 +0200

    update

---
+++ _ParaAlumnos_JS/js/script.js
@@ -0,0 +1,35 @@
+function blanco(){
+    document.getElementById("h2").style.backgroundColor = "white"
+}
+
+function rojo() {
+    document.getElementById("h2").style.backgroundColor = "red"
+}
+
+function naranja() {
+    document.getElementById("h2").style.backgroundColor = "orange"
+}
+
+function amarillo() {
+    document.getElementById("h2").style.backgroundColor = "yellow"
+}
+
+function verde() {
+    document.getElementById("h2").style.backgroundColor = "green"
+}
+
+function azul() {
+    document.getElementById("h2").style.backgroundColor = "blue"
+}
+
+function morado() {
+    document.getElementById("h2").style.backgroundColor = "purple"
+}
+
+function rosa() {
+    document.getElementById("h2").style.backgroundColor = "pink"
+}
+
+function gris() {
+    document.getElementById("h2").style.backgroundColor = "grey"
+}
